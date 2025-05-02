b5bc7f7eb9030b4bc69623d38f96e87ba7cdfb5a
Ankit Gupta <guptankit.2003@gmail.com>
2025-05-02 20:57:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-300">
+      <div className="text-3xl font-bold underline text-red-600">Ankit Gupta's Portfolio</div>
+    </div>
+  );
+}
+
+export default App
