182bae7752143a7150b085de2e3a6d96d7bcb950
Ankit Gupta <guptankit.2003@gmail.com>
2025-06-09 11:29:36 +0530

    Add Hero component and update portfolio title; remove unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from './componenets/Hero';
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-300">
-      <div className="text-3xl font-bold underline text-red-600">Ankit Gupta's Portfolio</div>
-    </div>
+    <>
+    <Hero />
+    </> 
   );
 }
 
